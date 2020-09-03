765dbdd0c5556c7eecc64f4c5a2ca333b12a7c1e
Anh Duy <anhduy.nguyen0211@gmail.com>
2020-09-03 10:30:47 +0700

    new uploader

--- backend/src/router/uploader.ts
+++ backend/src/router/uploader.ts
@@ -17,7 +17,7 @@ router.post('/',async (req,resp)=>{
     const {uploadDir} = req.body
 
 
-    for (let fileKey of Object.keys(req.files)) {
+    for (let fileKey of Object.keys(req.files)) { 
         let file = <expressFileupload.UploadedFile>req.files[fileKey];
         // let hashName = uuid();
         //ts-ignore
