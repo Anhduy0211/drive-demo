0ddb4fa0de6ee8bd3cefb2ebdaebda25797a75ba
Anh Duy <anhduy.nguyen0211@gmail.com>
2020-09-03 10:32:32 +0700

    add explaination

--- backend/src/ults/wirteFile.ts
+++ backend/src/ults/wirteFile.ts
@@ -3,7 +3,7 @@ import evn = require('../../environment')
 
 
 export function writeFileToDir(uuidName:string,data,directory:string){ 
-    fs.ensureDir(evn.environment.warehouse+'/'+directory).then(()=>{
+    fs.ensureDir(evn.environment.warehouse+'/'+directory).then(()=>{ // If dicrectory not exits then created and append the file
         fs.writeFile(evn.environment.warehouse+'/'+uuidName,data).then((vaule)=>{
             console.log(`Done upload : ${uuidName}`)
             
